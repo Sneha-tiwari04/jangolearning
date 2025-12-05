7a0e45bcc95623bb4a767c82a068d3846be9a0f7
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-12-05 20:17:23 +0530

    file

---
+++ Static/static_outside_app/project/app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+def landing1(req):
+    return render(req,'landing1.html')
+def landing(req):
+    return render(req,'landing.html')
+
+# Create your views here.
