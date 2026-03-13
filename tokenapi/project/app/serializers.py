ef488800193c45904c34d55d0a4571118e5990d0
kiran dwivedi <snehatiwari7654321@gmail.com>
2026-03-13 12:39:48 +0530

    file

--- tokenapi/project/app/serializers.py
+++ tokenapi/project/app/serializers.py
@@ -5,4 +5,4 @@ from .models import Employee
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ["id", "emp_name", "emp_email", "emp_department", "emp_role"]
+        fields = ["id", "emp_name", "emp_email", "emp_department", "emp_role" ]
